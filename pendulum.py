fe4f1d7d85d9cfe565c20920ca29afdd6742c059
Hector G. de Marina <noeth3r@gmail.com>
2019-05-04 18:22:26 +0200

    disturbance on angular velocity

--- pendulum.py
+++ pendulum.py
@@ -51,8 +51,10 @@ fig, ax = pl.subplots()
 
 for t in time:
     # Add small disturbances if you want, to check that the controller is working
+    # A disturbance can be a small change in the angular velocity due to wind for example
     # If the disturbance is BIG, then k_12 and k_11 must be also big, or far from the limit conditions of stability, i.e, we need big lambdas :P.
-    #X[0][0] = X[0][0] + 0.001*(0.5 - np.random.randn(1))
+    
+    # X[1][0] = X[1][0] + 0.0005*(0.5 - np.random.randn(1))
 
     # Controller for C = I
     # K = [k11 k12]
