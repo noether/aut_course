d130c26f6fd14b73ca5d7e4ae797d3708cf25f10
Hector G. de Marina <noeth3r@gmail.com>
2019-05-04 16:45:35 +0200

    controller for inverted pendulum

--- pendulum.py
+++ pendulum.py
@@ -19,14 +19,14 @@ g = 9.8 # Gravity acceleration [m/s/s]
 b = 1 # Friction coefficient [m*l*l/s]
 
 # Initial conditions
-theta = 0.01 # [rad]
-dot_theta = 0 # [rad/s]
+theta = 0.001 # [rad]
+dot_theta = -0.0 # [rad/s]
 
-X = np.array([theta, dot_theta]) # Construct the state-vector
+X = np.array([[theta], [dot_theta]]) # Construct the state-vector
 
 # Simulation parameters
-Tf = 300 # Final time [s]
-DT = 0.01 # Step time [s]
+Tf = 30 # Final time [s]
+DT = 0.001 # Step time [s]
 
 time = np.linspace(0, Tf, Tf/DT) # Time vector for plotting
 time_log = np.append(time, Tf+DT) # For the last iteration
@@ -34,9 +34,9 @@ it = 0 # Iteration index for logging
 plot_frame = 100 # We plot the state every plot_frame iterations
 
 # Data log
-X_h = np.zeros((time_log.size, X.size)) # We will store the values of the state vector here
+X_h = np.zeros((X.size, time_log.size)) # We will store the values of the state vector here
 
-X_h[it,:] = X # We log the first iteration before the loop starts
+X_h[:,it] = X.T # We log the first iteration before the loop starts
 
 pl.close("all")
 pl.ion()
@@ -44,17 +44,28 @@ fig, ax = pl.subplots()
 
 for t in time:
 
-    T = 0 # Control action
+    # Controller for C = I
+    # K = [k11 k12]
+    k12 = b - 1
+    k11 = -g*l*m - 0.25    # Stable
+    # k11 = -g*l*m + 0.25  # Inestable
 
-    dot_dot_theta = 1.0/(m*l*l) * (m*g*l*X[0] - b*X[1] + T) # Dynamics
+    B = np.array([[0],[1]])
+    C = np.eye(2)
+    K = np.array([k11, k12])
+    u = K.dot(C).dot(X) # u = KC X
 
-    X[1] = X[1] + dot_dot_theta*DT # Euler integration
-    X[0] = X[0] + X[1]*DT # Euler integration
+    T = u # Control action
+
+    dot_dot_theta = 1.0/(m*l*l) * (m*g*l*X[0][0] - b*X[1][0] + T) # Dynamics
+
+    X[1][0] = X[1][0] + dot_dot_theta*DT # Euler integration
+    X[0][0] = X[0][0] + X[1][0]*DT # Euler integration
 
     # Animation
     if it%plot_frame == 0:
         ax.clear()
-        ax.arrow(0, 0, l*np.sin(X[0]), l*np.cos(X[0]))
+        ax.arrow(0, 0, l*np.sin(X[0][0]), l*np.cos(X[0][0]))
         ax.set_xlim(-l*2, l*2)
         ax.set_ylim(-l*2, l*2)
 
@@ -73,16 +84,16 @@ for t in time:
     X[0] = normalizeAngle(X[0])
 
     # Log
-    X_h[it,:] = X
+    X_h[:,it] = X.T
 
 pl.figure(2)
-pl.plot(time_log, X_h[:, 0], label="$\theta$")
+pl.plot(time_log, X_h[0, :], label="$\theta$")
 pl.xlabel("Time [s]")
 pl.ylabel("Angle [rad]")
 pl.grid()
 
 pl.figure(3)
-pl.plot(time_log, X_h[:, 1], label="$\dot\theta$")
+pl.plot(time_log, X_h[0, :], label="$\dot\theta$")
 pl.xlabel("Time [s]")
 pl.ylabel("Angular velocity [rad/s]")
 pl.grid()
